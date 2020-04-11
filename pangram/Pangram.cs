c5c821fc7dea9734d802c0172b0c8c67f032acaf
Rodrigo Oliveira <rodrigo.oliveira@engenhos.net>
2020-04-11 02:59:32 -0300

    Pangram

---
+++ pangram/Pangram.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+
+public static class Pangram
+{
+    public static bool IsPangram(string input)
+    {
+        var result = from char v in "abcdefghijklmnopqrstuvxyz"
+                     where !input.ToLower().Contains(v)
+                     select v;
+
+        return !result.Any();
+    }
+}
