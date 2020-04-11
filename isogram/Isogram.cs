ec34bda8b59e7386ab0fc56a9f010959ecd57159
Rodrigo Oliveira <rodrigo.oliveira@engenhos.net>
2020-04-11 02:59:45 -0300

    Isogram

---
+++ isogram/Isogram.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+public static class Isogram
+{
+    public static bool IsIsogram(string word)
+    {
+        int ocurrences = 0;
+        var lowerWord = word.ToLowerInvariant();
+
+        foreach (var letter in lowerWord)
+        {
+            ocurrences += (from char l in lowerWord
+                           where letter == l && letter != '-' && letter != ' '
+                           select l).Count();
+            if (ocurrences > 1) return false;
+            ocurrences = 0;
+        }
+        return ocurrences == 0;
+    }
+}
