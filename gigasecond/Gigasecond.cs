dc639b76e8391b0cbc6b47b03e5845a60a75953b
Rodrigo Oliveira <rodrigo.oliveira@engenhos.net>
2020-05-15 17:32:06 -0300

    Uso de expression-bodied

--- gigasecond/Gigasecond.cs
+++ gigasecond/Gigasecond.cs
@@ -4,8 +4,5 @@ using static System.Math;
 
 public static class Gigasecond
 {
-    public static DateTime Add(DateTime moment)
-    {
-        return moment.AddSeconds(Pow(10, 9));
-    }
+    public static DateTime Add(DateTime moment) => moment.AddSeconds(Pow(10, 9));
 }
