540cd2a97310c140d986436e92e4bf9299932cea
Rodrigo Oliveira <rodrigodosanjosoliveira@gmail.com>
2020-10-11 19:51:14 -0300

    resistor-color

---
+++ resistor-color/ResistorColor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public static class ResistorColor
+{
+    public static int ColorCode(string color)
+    {
+        return Array.IndexOf(Colors(), color);
+    }
+
+    public static string[] Colors()
+    {
+        return new string[]
+        {
+            "black",
+            "brown",
+            "red",
+            "orange",
+            "yellow",
+            "green",
+            "blue",
+            "violet",
+            "grey",
+            "white"
+        };
+    }
+}
